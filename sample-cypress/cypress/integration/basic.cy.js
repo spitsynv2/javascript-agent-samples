9dbc00faee3a1ee6e621b4f06248a12fe6d6808b
spitsynv2 <61032832+spitsynv2@users.noreply.github.com>
2025-08-27 11:15:50 +0200

    Update basic.cy.js

--- sample-cypress/cypress/integration/basic.cy.js
+++ sample-cypress/cypress/integration/basic.cy.js
@@ -17,6 +17,38 @@ describe('Basic reporting test', () => {
     cy.xpath("//*[@id='search']//a").should('have.text', searchValue);
   });
 
+    it('Google search should be passed2', () => {
+    const searchValue = 'Zebrunner';
+    performGoogleSearch(searchValue);
+
+    console.log(`Verify first search result contains search value`);
+    cy.xpath("//*[@id='search']//a").should('contain.text', searchValue);
+  });
+
+    it('Google search should be passed3', () => {
+    const searchValue = 'Zebrunner';
+    performGoogleSearch(searchValue);
+
+    console.log(`Verify first search result contains search value`);
+    cy.xpath("//*[@id='search']//a").should('contain.text', searchValue);
+  });
+
+    it('Google search should be passed4', () => {
+    const searchValue = 'Zebrunner';
+    performGoogleSearch(searchValue);
+
+    console.log(`Verify first search result contains search value`);
+    cy.xpath("//*[@id='search']//a").should('contain.text', searchValue);
+  });
+  
+    it('Google search should be passed5', () => {
+    const searchValue = 'Zebrunner';
+    performGoogleSearch(searchValue);
+
+    console.log(`Verify first search result contains search value`);
+    cy.xpath("//*[@id='search']//a").should('contain.text', searchValue);
+  });
+
   function performGoogleSearch(searchValue) {
     cy.visit(url).contains('Google');
 
